48061763f14c24c744fee96d84e90a8e0850e5ba
Fabian De La Pena Montero <fdelapenamontero@bootcampspot.com>
2023-12-03 15:40:45 -0700

    Fixing dependency references so project runs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
